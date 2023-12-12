5023b51dd62a78b29353dbd882bd99b9c3c29a8f
Satishghode <satishghode9075@gmail.com>
2023-12-12 08:00:22 +0530

    03_Basic

---
+++ 03_Basic/01_for_of.js
@@ -0,0 +1,79 @@
+const arr = [1, 3, 4, 5, 5, 6, 7];
+
+for (const array of arr) {
+    // console.log(array);
+}
+
+
+const names = "satish";
+for (const name of names) {
+    // console.log(name);
+}
+
+for (const iterator of arr) {
+    // console.log(iterator);
+}
+
+//applied a for of loop in map 
+const map = new Map();
+
+map.set('IN','India');
+map.set('USA','United state of America');
+map.set('Fra','France');
+
+for (const maps of map) {
+    // console.log(maps);
+
+}
+
+
+// applied a forin Or forof loop on Array Object .
+
+const obj =[
+    {
+        name : 'satish',
+        midName: 'Bhasker',
+        sirName: 'Ghode',
+        phoneNumber: 90754114838,
+        location: 'Sukewadi tal.Sangamner Dis A-Ngar '
+    },
+    {
+        name : 'Jyoti',
+        midName: 'Bhasker',
+        sirName: 'Ghode',
+        phoneNumber: 90754114838,
+        location: 'Sukewadi tal.Sangamner Dis A-Ngar '
+    },
+    {
+        name : 'swati',
+        midName: 'Bhasker',
+        sirName: 'Ghode',
+        phoneNumber: 90754114838,
+        location: 'Sukewadi tal.Sangamner Dis A-Ngar '
+    },
+    {
+        name : 'Sangita',
+        midName: 'Bhasker',
+        sirName: 'Ghode',
+        phoneNumber: 90754114838,
+        location: 'Sukewadi tal.Sangamner Dis A-Ngar '
+    },
+
+]
+
+
+for (const object of obj) {
+    console.log(object);   
+}
+
+
+const myObject = {
+        name : 'satish',
+        midName: 'Bhasker',
+        sirName: 'Ghode',
+        phoneNumber: 90754114838,
+        location: 'Sukewadi tal.Sangamner Dis A-Ngar '
+}
+
+
+
